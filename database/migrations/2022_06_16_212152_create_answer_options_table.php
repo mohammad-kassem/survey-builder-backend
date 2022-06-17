dd3b230006fac0abca79437bb34d07e8943232c0
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-17 03:48:32 +0300

    feat: implementing the answer_options table migration

---
+++ database/migrations/2022_06_16_212152_create_answer_options_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswerOptionsTable extends Migration{
+    public function up()
+    {
+        Schema::create('avialable_answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('option', 255);
+            $table->integer('question_id');
+            $table->timestamps();
+        });
+    }
+
+    public function down(){
+        Schema::dropIfExists('avialable_answers');
+    }
+}
