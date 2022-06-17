3cb20799729a40d6385f4268fcd93576cca86ad8
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-17 03:48:54 +0300

    feat: implementing the user_answers table migration

---
+++ database/migrations/2022_06_16_212501_create_user_answers_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAnswersTable extends Migration{
+    public function up()
+    {
+        Schema::create('user_answers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('survey_id');
+            $table->integer('quetion_id');
+            $table->integer('user_id');
+            $table->string('value', 255);
+            $table->timestamps();
+        });
+    }
+
+    public function down(){
+        Schema::dropIfExists('user_answers');
+    }
+}
