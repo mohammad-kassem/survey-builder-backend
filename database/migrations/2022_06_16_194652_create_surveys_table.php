cb4ab0da5505c208552c6e7d0e94abf09b5218a2
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-17 03:47:51 +0300

    feat: implementing the surveys table migration

---
+++ database/migrations/2022_06_16_194652_create_surveys_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSurveysTable extends Migration{
+    public function up(){
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    public function down(){
+        Schema::dropIfExists('surveys');
+    }
+}
