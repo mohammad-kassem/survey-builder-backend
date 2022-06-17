83d73025789f1bc9e290cb75e2d2d524a0619578
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-17 20:30:10 +0300

    faet: setting up the jwt riken authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable{
+class User extends Authenticatable implements JWTSubject{
     use HasFactory, Notifiable;
     
     protected $fillable = [
@@ -36,5 +36,14 @@ class User extends Authenticatable{
         return $this->belongsTo(Role::class);
     }
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     
 }
